e9e5ade69ffa79c613e54fe0e55de7d04c310fa0
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-08-16 20:49:33 -0400

    Update read-email example to use vanilla server

--- packages/read-emails/server/node/server.js
+++ packages/read-emails/server/node/server.js
@@ -1,7 +1,6 @@
-const fastify = require('fastify');
-const cors = require('@fastify/cors');
 const dotenv = require('dotenv');
 const { mockDb } = require('./utils/mock-db');
+const { mockServer, getReqBody } = require('./utils/mock-server');
 const { prettyPrintJSON } = require('./utils/formatting');
 
 const Nylas = require('nylas');
@@ -35,7 +34,7 @@ nylasClient
       'Application whitelisted. Application Details: ',
       prettyPrintJSON(applicationDetails)
     );
-    startFastify();
+    startServer();
   });
 
 const exchangeMailboxTokenCallback = async (accessTokenObj, res) => {
@@ -51,62 +50,60 @@ const exchangeMailboxTokenCallback = async (accessTokenObj, res) => {
   });
 
   // Return an authorization object to the user
-  res.json({
-    id: user.id,
-    emailAddress: user.emailAddress,
-  });
+  res.writeHead(200).end(
+    JSON.stringify({
+      id: user.id,
+      emailAddress: user.emailAddress,
+    })
+  );
 };
 
-const startFastify = () => {
+const startServer = () => {
   // Use the routes provided by the Nylas Node SDK to quickly implement the authentication flow
   const { buildAuthUrl, exchangeCodeForToken } = NylasRoutes(nylasClient);
 
-  // Init the fastify server
-  const app = fastify();
+  // Configure the Nylas routes using your flavour of backend framework
+  mockServer.post(DefaultPaths.buildAuthUrl, async (req, res) => {
+    const body = await getReqBody(req);
 
-  // Enable CORS
-  app.register(cors);
-
-  // Configure the Nylas routes using your flavour of backend framework, fastify in this case
-  app.post(DefaultPaths.buildAuthUrl, async (req, res) => {
     const authUrl = await buildAuthUrl({
       scopes: [Scope.EmailReadOnly],
-      emailAddress: req.body.email_address,
-      successUrl: req.body.success_url,
+      emailAddress: body.email_address,
+      successUrl: body.success_url,
       clientUri,
     });
-    res.status(200).send(authUrl);
+
+    res.writeHead(200).end(authUrl);
   });
 
-  app.post(DefaultPaths.exchangeCodeForToken, async (req, res) => {
+  mockServer.post(DefaultPaths.exchangeCodeForToken, async (req, res) => {
+    const body = await getReqBody(req);
+
     try {
-      const accessTokenObj = await exchangeCodeForToken(req.body.token);
+      const accessTokenObj = await exchangeCodeForToken(body.token);
 
       await exchangeMailboxTokenCallback(accessTokenObj, res);
     } catch (e) {
-      res.status(500).send(e.message);
+      res.writeHead(500).end(e.message);
     }
   });
 
-  // Add some routes for the backend
-  app.get('/', (req, res) => res.status(200).send('Ok'));
-
   // Add route for getting 5 latest emails
-  app.get('/nylas/read-emails', async (req, res) => {
+  mockServer.get('/nylas/read-emails', async (req, res) => {
     if (!req.headers.authorization) {
-      return res.json('Unauthorized');
+      return res.writeHead(401).end('Unauthorized');
     }
 
     const user = await mockDb.findUser(req.headers.authorization);
     if (!user) {
-      return res.json('Unauthorized');
+      return res.writeHead(401).end('Unauthorized');
     }
 
     const messages = await nylasClient
       .with(user.accessToken)
       .messages.list({ limit: 5 });
 
-    return res.send(messages);
+    return res.writeHead(200).end(JSON.stringify(messages));
   });
 
   if (process.env.NODE_ENV === 'development') {
@@ -131,5 +128,6 @@ const startFastify = () => {
   }
 
   // Start listening on port 9000
-  app.listen({ port }).then(() => console.log('App listening on port' + port));
+  mockServer.init().listen(port);
+  console.log('App listening on port ' + port);
 };
