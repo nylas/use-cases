2f52b5a19df46c8e0a57bf838b2e23302003bd66
Heat Hamilton <55773810+heathergh@users.noreply.github.com>
2022-09-08 15:45:44 -0400

    [send-and-read-emails] Duplicate logging when a message is sent (#19)

--- packages/send-and-read-emails/client/react/src/App.js
+++ packages/send-and-read-emails/client/react/src/App.js
@@ -163,7 +163,6 @@ function SendEmails({ userId }) {
 
       const data = await res.json();
 
-      console.log(data);
       return data;
     } catch (e) {
       console.warn(`Error sending emails:`, e);
