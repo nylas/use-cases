195c32d9867a1b7312ac357a11d18bf6994f88b9
Pooja Guggari <pooja169usp@gmail.com>
2022-09-30 22:39:46 +0530

    [send-emails]: Fixed refreshing page resulting in loss of connected account info issue (#42)

--- packages/send-emails/server/node/utils/mock-db.js
+++ packages/send-emails/server/node/utils/mock-db.js
@@ -1,35 +1,87 @@
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const users = [];
 
-const mockDb = {
-  findUser: async (id) => {
-    return users.find((u) => u.id === id);
-  },
-  updateUser: async (userId, payload) => {
-    const idx = users.findIndex((u) => u.id === userId);
+class MockDB {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Filename is required');
+    }
+    this.filename = filename;
+    try {
+      fs.accessSync(filename, fs.constants.R_OK | fs.constants.W_OK);
+    } catch (err) {
+      fs.writeFileSync(filename, '[]');
+    }
+  }
+
+  async getJSONRecords() {
+    // Read filecontents of the datastore
+    const jsonRecords = await fs.promises.readFile(this.filename, {
+      encoding: 'utf8',
+    });
+    // Parse JSON records in JavaScript
+    return JSON.parse(jsonRecords);
+  }
+
+  // Logic to find data
+  async findUser(id, emailAddress) {
+    const jsonRecords = await this.getJSONRecords();
+    return jsonRecords.find(
+      (r) => r.emailAddress === emailAddress || r.id === id
+    );
+  }
+
+  // Logic to update data
+  async updateUser(id, payload) {
+    const jsonRecords = await this.getJSONRecords();
+
+    const idx = jsonRecords.findIndex((r) => r.id === id);
     if (idx === -1) {
-      throw new Error('User not found');
+      throw new Error('Record not found');
     }
 
-    users[idx] = { ...users[idx], ...payload };
-    return users[idx];
-  },
-  createUser: async (payload) => {
+    // Update existing record
+    jsonRecords[idx] = { ...jsonRecords[idx], ...payload };
+
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(jsonRecords, null, 2)
+    );
+    return jsonRecords[idx];
+  }
+
+  // Logic to add data
+  async createUser(payload) {
+    const jsonRecords = await this.getJSONRecords();
+
     const user = {
       id: uuidv4(),
       ...payload,
     };
-    users.push(user);
-    return user;
-  },
-  createOrUpdateUser: async (emailAddress, payload) => {
-    const user = await mockDb.findUser(emailAddress);
-    if (user) {
-      return await mockDb.updateUser(user.id, payload);
-    } else {
-      return await mockDb.createUser(payload);
-    }
-  },
-};
+    // Adding new record
+    jsonRecords.push(user);
 
-module.exports = { mockDb };
+    // Writing all records back to the file
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(jsonRecords, null, 2)
+    );
+
+    return user;
+  }
+
+  // Logic to add or update data
+  async createOrUpdateUser(id, attributes) {
+    const record = await this.findUser(id, attributes?.emailAddress);
+    if (record) {
+      return await this.updateUser(record.id, attributes);
+    } else {
+      return await this.createUser(attributes);
+    }
+  }
+}
+
+const mockDB = new MockDB('datastore.json');
+
+// 'datastore.json' is created at runtime
+module.exports = mockDB;
