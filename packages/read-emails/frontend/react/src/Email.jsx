f4539a4bf97b04717c1c239766bf01d0c0e0a2ff
YIFAN WU <yifan.w@nylas.com>
2022-11-01 20:03:38 -0400

    Added email list and preview item

--- packages/read-emails/frontend/react/src/Email.jsx
+++ packages/read-emails/frontend/react/src/Email.jsx
@@ -1,24 +1,49 @@
-import { styles } from './styles';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import EmailList from './EmailList';
+import './styles/email.scss';
 
-function Email({ thread, isOpen, setOpenEmail }) {
+function Email({ serverBaseUrl, userId }) {
+  const [emails, setEmails] = useState([]);
+  const [selectedEmail, setSelectedEmail] = useState('');
+
+  useEffect(() => {
+    const getEmails = async () => {
+      try {
+        const url = serverBaseUrl + '/nylas/read-emails';
+        const res = await fetch(url, {
+          method: 'GET',
+          headers: {
+            Authorization: userId,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await res.json();
+
+        setEmails(data);
+      } catch (e) {
+        console.warn(`Error retrieving emails:`, e);
+        return false;
+      }
+    };
+
+    getEmails();
+  }, [serverBaseUrl, userId]);
+
   return (
-    <li
-      key={thread.id}
-      onClick={() => (isOpen ? setOpenEmail('') : setOpenEmail(thread.id))}
-      style={styles.Email.container}
-    >
-      <div>{thread.subject}</div>
-      <div style={styles.Email.date}>
-        {new Date(Math.floor(thread.date * 1000)).toDateString()}
+    <div className="email-app">
+      <EmailList emails={emails} setSelectedEmail={setSelectedEmail} />
+      <div className="email-detail-view">
+        <h3>Email Details</h3>
+        <span>{selectedEmail.subject}</span>
       </div>
-      <div style={styles.Email.snippet}>{thread.snippet}</div>
-      {isOpen && (
-        <pre style={styles.Email.thread}>
-          <code>{JSON.stringify(thread, null, 4)}</code>
-        </pre>
-      )}
-    </li>
+    </div>
   );
 }
+
+Email.propTypes = {
+  serverBaseUrl: PropTypes.string.isRequired,
+  userId: PropTypes.string.isRequired,
+};
 
 export default Email;
