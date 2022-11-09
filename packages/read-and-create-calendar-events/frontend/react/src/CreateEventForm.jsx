a780f4ef49cfaec5ea7e6187fd5c6f07d3d95ad3
Jay Gudsson <gudsson@gmail.com>
2022-11-09 00:28:41 -0700

    adds date fields

--- packages/read-and-create-calendar-events/frontend/react/src/CreateEventForm.jsx
+++ packages/read-and-create-calendar-events/frontend/react/src/CreateEventForm.jsx
@@ -8,51 +8,51 @@ import {
 } from './utils/date';
 
 function CreateEventForm({ setShowCreateEventForm }) {
-  const [startTime, setStartTime] = useState(currentTime());
-  const [endTime, setEndTime] = useState(currentTimePlusHalfHour());
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
   const now = new Date();
 
-  const createEvent = async (e) => {
-    e.preventDefault();
+  // const createEvent = async (e) => {
+  //   e.preventDefault();
 
-    try {
-      const url = serverBaseUrl + '/nylas/create-events';
+  //   try {
+  //     const url = serverBaseUrl + '/nylas/create-events';
 
-      const res = await fetch(url, {
-        method: 'POST',
-        headers: {
-          Authorization: userId,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          startTime: applyTimezone(startTime),
-          endTime: applyTimezone(endTime),
-          title,
-          description,
-          calendarId,
-        }),
-      });
+  //     const res = await fetch(url, {
+  //       method: 'POST',
+  //       headers: {
+  //         Authorization: userId,
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         startTime: applyTimezone(startTime),
+  //         endTime: applyTimezone(endTime),
+  //         title,
+  //         description,
+  //         calendarId,
+  //       }),
+  //     });
 
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
+  //     if (!res.ok) {
+  //       throw new Error(res.statusText);
+  //     }
 
-      const data = await res.json();
+  //     const data = await res.json();
 
-      console.log('Event created:', data);
+  //     console.log('Event created:', data);
 
-      // reset form fields
-      setStartTime(getLocalDateString(new Date()));
-      setEndTime(getLocalDateString(new Date()));
-      setTitle('');
-      setDescription('');
-    } catch (err) {
-      console.warn(`Error creating event:`, err);
-    }
-  };
+  //     // reset form fields
+  //     setStartTime(getLocalDateString(new Date()));
+  //     setEndTime(getLocalDateString(new Date()));
+  //     setTitle('');
+  //     setDescription('');
+  //   } catch (err) {
+  //     console.warn(`Error creating event:`, err);
+  //   }
+  // };
 
   return (
     <div className="create-event-view">
@@ -69,6 +69,78 @@ function CreateEventForm({ setShowCreateEventForm }) {
           <button className="blue">Create</button>
         </div>
       </div>
+      <form>
+        <div className="row">
+          <div className="field-container">
+            <label htmlFor="event-title">Event title</label>
+            <input
+              type="text"
+              name="event title"
+              placeholder="Discuss calendar APIs"
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+              value={title}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="field-container">
+            <label htmlFor="event-start-time">Start time</label>
+            <input
+              type="datetime-local"
+              name="event-start-time"
+              className={startTime === '' ? 'placeholder' : ''}
+              onChange={(event) => {
+                setStartTime(event.target.value);
+              }}
+              value={startTime}
+              min={getLocalDateString(now)}
+            />
+          </div>
+          <div className="field-container">
+            <label htmlFor="event-end-time">End time</label>
+            <input
+              type="datetime-local"
+              name="event-end-time"
+              className={endTime === '' ? 'placeholder' : ''}
+              onChange={(event) => {
+                setEndTime(event.target.value);
+              }}
+              value={endTime}
+              min={getLocalDateString(now)}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="field-container">
+            <label htmlFor="event-start-time">Start time</label>
+            <input
+              type="datetime-local"
+              name="event-start-time"
+              onChange={(event) => {
+                setStartTime(event.target.value);
+              }}
+              value={startTime}
+              min={getLocalDateString(now)}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="field-container">
+            <label htmlFor="event-title">Event title</label>
+            <input
+              type="text"
+              name="event title"
+              placeholder="Discuss calendar APIs"
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+              value={title}
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 
