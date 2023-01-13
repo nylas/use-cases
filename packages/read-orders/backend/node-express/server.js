8530f9ed96c7bc2654b938c1d818f97073d42c0d
Jay Gudsson <43771331+gudsson@users.noreply.github.com>
2023-01-13 15:51:42 -0700

    [read-orders] code cleanup (#100)
    * remove mock data
    
    * clean server.js
    
    * clean up comments
    
    * more cleanup
    
    * move helpers to /utils
    
    * move getRawMessage func to /utils
    
    * move useEffect

--- packages/read-orders/backend/node-express/server.js
+++ packages/read-orders/backend/node-express/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mockDb = require('./utils/mock-db');
+const {
+  isOrderEmail,
+  prepEmailForParser,
+  getRawMessage,
+} = require('./utils/email-helpers');
 const axios = require('axios');
 
 const Nylas = require('nylas');
@@ -56,41 +61,6 @@ const expressBinding = new ServerBindings.express(nylasClient, {
   clientUri: CLIENT_URI,
 });
 
-function isOrderEmail(message) {
-  return ['order', 'purchase', 'tracking'].some((word) =>
-    message.subject.toLowerCase().includes(word)
-  );
-}
-
-function prepEmailForParser(messageObj, rawEmail) {
-  const senderEmail = isForwardedMessage(messageObj)
-    ? getOriginalSender(messageObj)
-    : messageObj.from[0].email;
-  const senderDomain = senderEmail.split('@')[1].replace(/.ca$/, '.com');
-
-  return {
-    emailTimestamp: +messageObj.date * 1000,
-    fetchedEmailId: messageObj.id,
-    from: senderEmail,
-    senderDomain: senderDomain,
-    textBase64: encodeBase64(rawEmail),
-  };
-
-  function isForwardedMessage(message) {
-    return message.snippet
-      .toLowerCase()
-      .includes('---------- forwarded message ---------');
-  }
-
-  function getOriginalSender(message) {
-    return message.snippet.match(/<(\S+@\S+)>/)[1];
-  }
-
-  function encodeBase64(data) {
-    return Buffer.from(data).toString('base64');
-  }
-}
-
 // Mount the express middleware to your express app
 const nylasMiddleware = expressBinding.buildMiddleware();
 app.use(nylasMiddleware);
@@ -108,13 +78,12 @@ expressBinding.startDevelopmentWebsocket().then((webhookDetails) => {
   console.log('Webhook tunnel registered. Webhook ID: ' + webhookDetails.id);
 });
 
-// WIP: Get messages and pass "order" emails to parser API
+// Get messages and pass relevant order emails to parser API
 app.get('/nylas/get-orders', async (req, res) => {
   if (!req.headers.authorization) {
     return res.json('Unauthorized');
   }
 
-  // We still provide access to the RFC-2822 raw message object by passing in Accept: message/rfc822 as the header.
   const user = await mockDb.findUser(req.headers.authorization);
 
   if (!user) {
@@ -125,17 +94,22 @@ app.get('/nylas/get-orders', async (req, res) => {
     .with(user.accessToken)
     .messages.list({ limit: 100, expanded: true });
 
-  let preppedEmails = await Promise.all(
+  // Prepare raw emails for sending to the Nylas parser API
+  const preppedEmails = await Promise.all(
     messages
       .filter(isOrderEmail)
       .map(async (message) =>
-        prepEmailForParser(message, await getRawMessage(message))
+        prepEmailForParser(
+          message,
+          await getRawMessage(message, nylasClient, user)
+        )
       )
   );
 
-  const url = `https://nylas-neural-parsers-test.us.nylas.com/parse_order`;
+  const parserUrl = `https://nylas-neural-parsers-test.us.nylas.com/parse_order`;
 
-  const parserResponse = await axios.post(url, {
+  // Send raw emails to order info to the Nylas parser API
+  const parserResponse = await axios.post(parserUrl, {
     metadata: { market: 'US' },
     emails: preppedEmails,
   });
@@ -155,220 +129,6 @@ app.get('/nylas/get-orders', async (req, res) => {
   return res.json(
     parsedResponses.filter((email) => email.email_category !== 'other')
   );
-
-  async function getRawMessage(messageObj) {
-    return await nylasClient
-      .with(user.accessToken)
-      .messages.findRaw(messageObj.id);
-  }
-});
-
-app.get('/nylas/get-mock-orders', async (req, res) => {
-  if (!req.headers.authorization) {
-    return res.json('Unauthorized');
-  }
-
-  // We still provide access to the RFC-2822 raw message object by passing in Accept: message/rfc822 as the header.
-  const user = await mockDb.findUser(req.headers.authorization);
-
-  if (!user) {
-    return res.json('Unauthorized');
-  }
-
-  const mockData = [
-    {
-      specversion: '1.0',
-      status: 'SUCCESS',
-      order: {
-        coupon: null,
-        currency: 'USD',
-        discount: null,
-        gift_card: null,
-        line_items: [
-          {
-            name: 'Jay, hayyyshayyy has dispatched your order!',
-            quantity: null,
-            unit_price: null,
-            product_id: null,
-            color: null,
-            size: null,
-            url: `https://help.etsy.com/hc/articles/360020601674?
-              campaign_label=shipping_notification_boe_convourl_treatment
-              &utm_source=transactional&utm_campaign=shipping_notification_boe_convourl_treatment_010170_433263510924_0_0
-              &utm_medium=email&utm_content=&email_sent=1640469464&euid=9Pr53Yl4PfY48SgbkTDkNq7fUJ3A&eaid=36678400514&x_eaid=61c3f1b72d&link_clicked=0`,
-            product_image_uri:
-              'https://i.etsystatic.com/7120425/r/il/821aa3/2792420144/il_170x135.2792420144_7dou.jpg',
-          },
-          {
-            name: 'Jay, hayyyshayyy has dispatched your order!',
-            quantity: null,
-            unit_price: null,
-            product_id: null,
-            color: null,
-            size: null,
-            url: `https://help.etsy.com/hc/articles/360020601674?
-            campaign_label=shipping_notification_boe_convourl_treatment
-            &utm_source=transactional&utm_campaign=shipping_notification_boe_convourl_treatment_010170_433263510924_0_0
-            &utm_medium=email&utm_content=&email_sent=1640469464&euid=9Pr53Yl4PfY48SgbkTDkNq7fUJ3A&eaid=36678400514&x_eaid=61c3f1b72d&link_clicked=0`,
-            product_image_uri:
-              'https://i.etsystatic.com/7120425/r/il/821aa3/2792420144/il_170x135.2792420144_7dou.jpg',
-          },
-        ],
-        order_date: '2022-12-05T16:55:02-08:00',
-        order_number: '2290912646',
-        shipping_total: null,
-        total_tax_amount: null,
-        total_amount: null,
-      },
-      merchant: {
-        name: 'Etsy',
-        domain: 'etsy.com',
-        tier: null,
-      },
-      fetched_email_id: 'abqqgqst36vrfhkz79zjprzgx',
-      email_timestamp: 1670288102000000,
-      error: null,
-      validation_errors: null,
-      metadata: {
-        account_id: null,
-        grant_id: null,
-        application_id: null,
-        email_category: null,
-        sync_category: null,
-        notification_timestamp: null,
-        sync_requested_at: null,
-        provider: null,
-        language: null,
-        market: 'US',
-        redirect: null,
-        memberId: null,
-        sender_domain: 'etsy.com',
-        message_id: null,
-        vendor: 'nylas',
-      },
-      email_category: 'shipment_confirmation',
-    },
-    {
-      specversion: '1.0',
-      status: 'SUCCESS',
-      order: {
-        coupon: null,
-        currency: 'USD',
-        discount: null,
-        gift_card: null,
-        line_items: [
-          {
-            name: 'Handmade Fabric Face Masks',
-            quantity: 1,
-            unit_price: null,
-            product_id: null,
-            color: null,
-            size: null,
-            url: `https://www.etsy.com/your/purchases/claim?
-            token=NTY5ODI4NDg4fDIyOTA5MTI2NDY%3D&signature=i4Go3j75LPZBBPGxzR3Bp2QLHQAq3RXb9cUlaVnZF9Q%3D
-            &campaign_label=proteus_transaction_guest_buyer_notification_v2&utm_source=transactional
-            &utm_campaign=proteus_transaction_buyer_notification_010170_239793797795_0_0&utm_medium=email
-            &from_email=order_confirmation&utm_content=&email_sent=1638674203&euid=AeIyt8YcvsBBW_FAG5JZZEydQ_u0
-            &eaid=36678400514&x_eaid=61c3f1b72d&link_clicked=4`,
-            product_image_uri:
-              'https://i.etsystatic.com/7120425/r/il/821aa3/2792420144/il_340x270.2792420144_7dou.jpg',
-          },
-          {
-            name: 'Handmade NHL Scrunchie',
-            quantity: 1,
-            unit_price: null,
-            product_id: null,
-            color: null,
-            size: null,
-            url: `https://www.etsy.com/your/purchases/claim?token=NTY5ODI4NDg4fDIyOTA5MTI2NDY%3D
-            &signature=i4Go3j75LPZBBPGxzR3Bp2QLHQAq3RXb9cUlaVnZF9Q%3D&campaign_label=proteus_transaction_guest_buyer_notification_v2
-            &utm_source=transactional&utm_campaign=proteus_transaction_buyer_notification_010170_239793797795_0_0&utm_medium=email
-            &from_email=order_confirmation&utm_content=&email_sent=1638674203&euid=AeIyt8YcvsBBW_FAG5JZZEydQ_u0
-            &eaid=36678400514&x_eaid=61c3f1b72d&link_clicked=6`,
-            product_image_uri:
-              'https://i.etsystatic.com/7120425/r/il/fbf5be/3311186749/il_340x270.3311186749_ppqp.jpg',
-          },
-        ],
-        order_date: '2022-12-05T16:54:20-08:00',
-        order_number: '2290912646',
-        shipping_total: 1359,
-        total_tax_amount: 0,
-        total_amount: 3367,
-      },
-      merchant: {
-        name: 'Etsy',
-        domain: 'etsy.com',
-        tier: null,
-      },
-      fetched_email_id: 'dw6d7remzj91gb8em8nh41q05',
-      email_timestamp: 1670288060000000,
-      error: null,
-      validation_errors: null,
-      metadata: {
-        account_id: null,
-        grant_id: null,
-        application_id: null,
-        email_category: null,
-        sync_category: null,
-        notification_timestamp: null,
-        sync_requested_at: null,
-        provider: null,
-        language: null,
-        market: 'US',
-        redirect: null,
-        memberId: null,
-        sender_domain: 'etsy.com',
-        message_id: null,
-        vendor: 'nylas',
-      },
-      email_category: 'order_confirmation',
-    },
-    {
-      specversion: '1.0',
-      status: 'SUCCESS',
-      order: {
-        coupon: null,
-        currency: 'USD',
-        discount: null,
-        gift_card: null,
-        line_items: [],
-        order_date: '2022-12-04T00:00:00-08:00',
-        order_number: '701-9291360-9438608',
-        shipping_total: null,
-        total_tax_amount: null,
-        total_amount: 196,
-      },
-      merchant: {
-        name: 'Amazon',
-        domain: 'amazon.com',
-        tier: null,
-      },
-      fetched_email_id: 'aapgh1o51qdekvli2hhd7nu41',
-      email_timestamp: 1670286411000000,
-      error: null,
-      validation_errors: null,
-      metadata: {
-        account_id: null,
-        grant_id: null,
-        application_id: null,
-        email_category: null,
-        sync_category: null,
-        notification_timestamp: null,
-        sync_requested_at: null,
-        provider: null,
-        language: null,
-        market: 'US',
-        redirect: null,
-        memberId: null,
-        sender_domain: 'amazon.com',
-        message_id: null,
-        vendor: 'nylas',
-      },
-      email_category: 'order_confirmation',
-    },
-  ];
-
-  return res.json(mockData);
 });
 
 // Before we start our backend, we should whitelist our frontend
